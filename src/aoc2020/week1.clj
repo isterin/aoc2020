5e3c589674325d1dccce3e8549d3dd9bc333ee71
Ilya Sterin <isterin@gmail.com>
2021-01-12 21:41:34 -0500

    Removed superfluous 'do' forms

--- src/aoc2020/week1.clj
+++ src/aoc2020/week1.clj
@@ -10,8 +10,7 @@
         l3 (rest l2)
         [a b] (first (pair-for l1 l2 2020))
         [d e f] (first (triple-for l1 l2 l3 2020))]
-    (do
-      [(* a b) (* d e f)])))
+    [(* a b) (* d e f)]))
 
 (defn- pair-for [l1 l2 sumto]
   (for [x l1
@@ -19,8 +18,7 @@
         :let [z (+ (Integer/parseInt x) (Integer/parseInt y))]
         :when (= z sumto)
         ]
-    (do
-      [(Integer/parseInt x) (Integer/parseInt y)])))
+    [(Integer/parseInt x) (Integer/parseInt y)]))
 
 (defn- triple-for [l1 l2 l3 sumto]
   (for [a l1
@@ -29,5 +27,4 @@
         :let [z (+ (Integer/parseInt a) (Integer/parseInt b) (Integer/parseInt c))]
         :when (= z sumto)
         ]
-    (do
-      [(Integer/parseInt a) (Integer/parseInt b) (Integer/parseInt c)])))
+    [(Integer/parseInt a) (Integer/parseInt b) (Integer/parseInt c)]))
