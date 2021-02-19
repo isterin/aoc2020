9cf9698aca7492c7c7ca7b427cb6b477024d26cb
Ilya Sterin <isterin@gmail.com>
2021-02-19 13:22:45 -0500

    Finished day 12

--- src/aoc2020/day12.clj
+++ src/aoc2020/day12.clj
@@ -8,7 +8,7 @@
             [loom.alg :as alg]
             [loom.alg-generic :as lag]))
 
-(declare calculate follow move move-forward turn move-to)
+(declare calculate follow move forward turn move-to ->DefaultMovement ->WaypointMovement)
 
 (defn calculate [input]
   (let [lines (core/read-resource-file input)
@@ -16,45 +16,82 @@
                        #(->> % (re-matches #"(\w)(\d+)") (drop 1))
                        lines)
         instructions' (map (fn [[dir unit]] [dir (Integer/parseInt unit)]) instructions)
-        position (follow instructions')]
-    [(+ (Math/abs (position :east)) (Math/abs (position :north))) 2]))
+        position (follow instructions' (->DefaultMovement {:north 0, :east 0, :current-dir :east}))
+        position2 (follow instructions' (->WaypointMovement {:north 0, :east 0,
+                                                             :waypoint-north 1, :waypoint-east 10}))]
+    (println position2)
+    (println (+ (Math/abs (position2 :east)) (Math/abs (position2 :north))))
+    [(+ (Math/abs (position :east)) (Math/abs (position :north)))
+     (+ (Math/abs (position2 :east)) (Math/abs (position2 :north)))]))
 
-(defn- follow [instructions]
+
+(defprotocol Movement
+  (forward [this unit])
+  (turn [this dir unit])
+  (move-to [this dir unit]))
+
+(defn- follow [instructions movement-impl]
   (loop [instructions instructions
-         position {:north 0, :east 0, :current-dir :east}]
+         movement-impl movement-impl]
     (if (first instructions)
-      (recur (rest instructions) (move (first instructions) position))
-      position)))
+      (recur (rest instructions) (move (first instructions) movement-impl))
+      (:position movement-impl))))
 
-(defn- move [[dir unit] position]
-  ;(println position)
+(defn- move [[dir unit] movement-impl]
   (let [new-pos (case dir
-                  "F" (move-forward unit position)
-                  ("L" "R") (turn dir unit position)
-                  ("E" "W" "S" "N") (move-to dir unit position))]
-    ;(println new-pos)
+                  "F" (forward movement-impl unit)
+                  ("L" "R") (turn movement-impl dir unit)
+                  ("E" "W" "S" "N") (move-to movement-impl dir unit))]
     new-pos))
 
-(defn- move-forward [unit position]
-  ;(println "move-forward" unit)
-  (case (position :current-dir)
-    :north (update position :north + unit)
-    :south (update position :north - unit)
-    :east (update position :east + unit)
-    :west (update position :east - unit)))
+; Default Movement implementation
 
-(defn- turn [dir unit position]
-  ;(println "turn" dir unit)
-  (let [circle (take 8 (cycle [:east :south :west :north]))
-        circle' (if (= dir "R") circle (reverse circle))
-        circle'' (drop-while #(not= % (position :current-dir)) circle')]
-    (assoc position :current-dir (first (drop (/ unit 90) circle'')))))
+(defrecord DefaultMovement [position]
+  Movement
+  (forward [this unit]
+    ;(println "move-forward" unit)
+    (case (position :current-dir)
+      :north (update-in this [:position :north] + unit)
+      :south (update-in this [:position :north] - unit)
+      :east (update-in this [:position :east] + unit)
+      :west (update-in this [:position :east] - unit)))
+  (turn [this dir unit]
+    (let [circle (take 8 (cycle [:east :south :west :north]))
+          circle' (if (= dir "R") circle (reverse circle))
+          circle'' (drop-while #(not= % (position :current-dir)) circle')]
+      (assoc-in this [:position :current-dir] (first (drop (/ unit 90) circle'')))))
+  (move-to [this dir unit]
+    (case dir
+      "N" (update-in this [:position :north] + unit)
+      "S" (update-in this [:position :north] - unit)
+      "E" (update-in this [:position :east] + unit)
+      "W" (update-in this [:position :east] - unit))))
 
-(defn- move-to [dir unit position]
-  ;(println "move-to" dir unit)
-  (case dir
-    "N" (update position :north + unit)
-    "S" (update position :north - unit)
-    "E" (update position :north + unit)
-    "W" (update position :north - unit)))
+
+; Waypoint Movement Implementation
+; position -> {:north :east :waypoint-north :waypoint-east}
+(defrecord WaypointMovement [position]
+  Movement
+  (forward [this unit]
+    ;(println "move-forward" unit)
+    (let [this' (update-in this [:position :north] + (* (position :waypoint-north) unit))
+          this'' (update-in this' [:position :east] + (* (position :waypoint-east) unit))]
+      this''))
+  (turn [this dir unit]
+    (let [radians (Math/toRadians (if (= dir "R") (- unit) unit))
+          x (-
+              (* (Math/cos radians) (position :waypoint-east))
+              (* (Math/sin radians) (position :waypoint-north)))
+          y (+
+              (* (Math/sin radians) (position :waypoint-east))
+              (* (Math/cos radians) (position :waypoint-north)))]
+      (-> this
+          (assoc-in [:position :waypoint-east] (Math/round x))
+          (assoc-in [:position :waypoint-north] (Math/round y)))))
+  (move-to [this dir unit]
+    (case dir
+      "N" (update-in this [:position :waypoint-north] + unit)
+      "S" (update-in this [:position :waypoint-north] - unit)
+      "E" (update-in this [:position :waypoint-east] + unit)
+      "W" (update-in this [:position :waypoint-east] - unit))))
 
--- test/aoc2020/day12_test.clj
+++ test/aoc2020/day12_test.clj
@@ -3,9 +3,18 @@
             [aoc2020.day12 :refer :all]))
 
 (deftest calculate-test
-  (testing "Day 11 test"
+  (testing "Day 12 test"
     (let [[a b] (calculate "day12_input1.txt")]
       (do
         (is (= a 1496))
-        (is (= b 2))
+        (is (= b 63843))
+        ))))
+
+
+(deftest calculate-sample-test
+  (testing "Day 12 test"
+    (let [[a b] (calculate "day12_input1_sample.txt")]
+      (do
+        (is (= a 25))
+        (is (= b 286))
         ))))
