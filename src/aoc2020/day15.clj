4b6072ac24ec630f256962b21cdd438fa8c8a920
Ilya Sterin <isterin@gmail.com>
2021-03-14 22:01:11 -0400

    Day 15 part 2

--- src/aoc2020/day15.clj
+++ src/aoc2020/day15.clj
@@ -1,7 +1,7 @@
 (ns aoc2020.day15)
 
 (defn calculate [input n]
-  ; We initialize the vector, since appending to it becomes a performance bottleneck for large n
+  ; We initialize the vector because growing it becomes a performance bottleneck for large n
   (loop [lst (apply vector-of :int (concat input (repeat (- n (count input)) 0)))
          idx (dec (count input))
          cache (into {} (map-indexed (fn [idx itm] [itm idx]) (drop-last input)))]
